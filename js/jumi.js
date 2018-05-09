9a616e0ecbe9b717055ad7835dc4774de6f03656
HighYitan <32370248+HighYitan@users.noreply.github.com>
2018-05-09 20:48:52 +0200

    Add files via upload

--- js/jumi.js
+++ js/jumi.js
@@ -1,1 +1,15 @@
-/*asdjasdoisa*/
+/*
+var counter = 0;
+var timer = setInterval(contar,1000);
+
+function contar(){
+
+	$("h1").html(counter);
+	if (counter==3){
+		counter=0;
+	}
+	else{
+		counter++
+	}
+}
+*/
